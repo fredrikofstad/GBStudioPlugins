0f61a7d367e857affbed08c4ca709353ecb56e9e
Fredrik Ofstad <nokotin@gmail.com>
2023-03-31 17:32:06 +0900

    updated guide with pictures

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Plugin Collection for GB Studio
+# Plugin Collection for GB Studio 3.1
 
 This is a collection (currently one) of plugins I've written for GB Studio.
 
@@ -16,12 +16,53 @@ This plugin is mostly a quality of life script as it replaces the time consuming
 Instead just plop a few values into the event script and you'll be animating tiles in no time!
 
 You can choose to change between 1-8 tiles, either 8x8 or 16x16 and how many frames you wish to be animated.
+For infinte loop animations this event should be put into an actor's update loop (alternatively a timer). The wait time between frames is entered directly into the event script.
 
-### Guide
+Besides animating your rivers and waterfalls, you can use this plugin for HUDs, menus, and other graphical changes to your scene.
 
-How to use Swap Tiles plugin
+//something about unique tiles
+
+### Guide - How to use the Swap Tiles plugin 
+
+For more information about tile swapping please read: [GB Studio central's guide](https://gbstudiocentral.com/tips/animating-background-tiles-3-1/) 
+
+First let's prepare our tilesheets. In order to swap tiles we first need to create a new scene with a tilesheet consisiting of all the tiles we want to swap. If you want to swap 16x16 tiles, the tiles have to be in the following order: Top left, top right, bottom left, bottom right. If you want several frames of animation the tiles have to come directly after eachother.
+
+![Tilesheet Preperation](https://github.com/fredrikofstad/GBStudioPlugins/res/preperation.png?raw=true)
+
+For our game scene we need to insert unique tiles where we want the tiles to be swapped, or else every tile that shares the same 8x8 image will change. In the picture you can see that I used the gbs-mono tileset.
+
+Now that everything is set up we can insert the script from Add Event->Plugins->Swap Tiles. If placed in an on init, the script will be run once. If you want a continous animation place the script in an actors On Update.
+
+- Tile Size is how big the tiles you want to swap are, either 8x8 or 16x16.
+
+- Frames of animation are how many frames are in your animation, for a one time swap input 1.
+
+- Number of tiles to be swapped are how many tiles you want to swap at the same time. In an update loop all these tiles will be swapped before moving to the next iteration.
+
+For every tile input the unique x and y coordinate from your game scene. If you chos 16x16 mode you only need to specify the top left tile. In my case the @ mark is x = 3 and y = 6. 
+Nexy input the x and y values of the tile you want to swap to from the swap-tileset. And once again, if you chose 16x16 mode you only need the first tile. In my case I want the image of an egg so I input x = 4 and y = 3.
+
+Frames to wait between swaps. If you are animating the tiles, this is where you specify the duration before swapping.
+
+Tilemap name. This is the name of the tilemap with the tiles you are swapping to. It's the filename of the picture in lowercase letters without the file extension. E.x. if you called your file SwapTileset.png, input swaptileset.
+
+Length of tileset is how many 8x8 tiles fit horizontally, and is when the script converts from coordinates to tile index. A 160x144 screen has 20 tiles, which is the default.
+
+![Result](https://github.com/fredrikofstad/GBStudioPlugins/res/result.png?raw=true)
+
+I hope this script speeds up your development time!
+
+The following scene took me around 5 minutes after setting up the tilesheet:
+
+![Result](https://github.com/fredrikofstad/GBStudioPlugins/res/animated.gif?raw=true)
+
+(Please excuse the gif compression)
+
+
+
 
 ## Download Plugins
 
-You can download a zip of all plugins in the repository here:
+You can download the plugin here:
 
--- plugins/SwapTiles/events/eventSwapTiles.js
+++ plugins/SwapTiles/events/eventSwapTiles.js
@@ -1,46 +1,151 @@
 const id = "FO_EVENT_SWAP_TILES";
-const groups = ["FO"];
+const groups = ["Plugins"];
 const name = "Swap Tiles";
 
-const fields = [
-    {
-      key: "x",
-      label: "X",
-      type: "number",
-      min: 0,
-      width: "50%",
-      defaultValue: 0,
-    },
-    {
-      key: "y",
-      label: "Y",
-      type: "number",
-      min: 0,
-      width: "50%",
-      defaultValue: 0,
-    },
+const MAX_TILES = 20;
+
+const fields = [].concat(
+  [
     {
       key: "tileAmount",
-      label: "Amount of Tiles",
+      label: "Tile Size",
+      description: "Choose the size of the tiles you wish to animate.",
       type: "select",
       width: "50%",
       options: [
-      ["single", "1x1"],
-      ["four", "2x2"]
+      ["single", "8x8"],
+      ["four", "16x16"]
       ],
       defaultValue: "single",
     },
     {
       key: "frames",
-      label: "Frame amount",
+      label: "Frames of animation",
+      description: "How many frames the animation has. Choose 1 for a one time tileswap.",
       type: "number",
       min: 1,
       width: "50%",
       defaultValue: 1,
     },
+
+    {
+      key: "items",
+      label: "Number of tiles to be swapped",
+      description: "How many unique tiles to be swapped in one loop",
+      type: "number",
+      min: 1,
+      max: MAX_TILES,
+      defaultValue: 1,
+    },
+    {
+      type: "break",
+    },
+
+    ...Array(MAX_TILES)
+      .fill()
+      .reduce((arr, _, i) => {
+        const index = i + 1;
+        arr.push(
+          {
+            type: "break",
+            conditions: [
+              {
+                key: "items",
+                gte: index,
+              },
+            ],
+          },
+          {
+            key: `tile${index}_x`,
+            type: "number",
+            label: `Unique Tile ${index}'s X`,
+            defaultValue: 0,
+            width: "50%",
+            conditions: [
+              {
+                key: "items",
+                gte: index,
+              },
+            ],
+          },
+          {
+            key: `tile${index}_y`,
+            type: "number",
+            label: `Unique Tile ${index}'s Y`,
+            defaultValue: 0,
+            width: "50%",
+            conditions: [
+              {
+                key: "items",
+                gte: index,
+              },
+            ],
+          },
+
+
+          {
+            type: "break",
+            conditions: [
+              {
+                key: "items",
+                gte: index,
+              },
+            ],
+          },
+
+          {
+            key: `swap${index}_x`,
+            label: `Tileset X of Tile ${index}`,
+            description: "X coordinate of the starting tile in the tileset you will be swapping to.",
+            type: "number",
+            min: 0,
+            width: "50%",
+            conditions: [
+              {
+                key: "items",
+                gte: index,
+              },
+            ],
+            defaultValue: 0,
+          },
+          {
+            key: `swap${index}_y`,
+            label: `Tileset Y of Tile ${index}`,
+            description: "Y coordinate of the starting tile in the tileset you will be swapping to.",
+            type: "number",
+            min: 0,
+            width: "50%",
+            conditions: [
+              {
+                key: "items",
+                gte: index,
+              },
+            ],
+            defaultValue: 0,
+          },
+
+          {
+            type: "break",
+            conditions: [
+              {
+                key: "items",
+                gte: index,
+              },
+            ],
+          },
+
+
+
+        );
+        return arr;
+      }, []),
+
+
+    
     {
       key: "waitFrames",
       label: "Frames to wait between swaps",
+      description: "The duration in frames until the next frame of animation is swapped.",
       type: "number",
       min: 0,
       width: "50%",
@@ -49,44 +154,62 @@ const fields = [
     {
       key: "tilemapName",
       label: "Tilemap Name",
+      // TODO: Check case sensitivity
+      description: "The tilemap name is the name of the file of the files you want to swap in lowercase without file extensions.",
       type: "text",
       defaultValue: "",
       flexBasis: "100%",
     },
     {
-      key: "tileMapIndex",
-      label: "Index in tilemap",
+      key: "tileLength",
+      label: "Length Of Tilemap",
+      description: "How many tiles your tilemap has in the X axis",
       type: "number",
       min: 0,
       width: "50%",
-      defaultValue: 0,
+      defaultValue: 20,
     },
 
-  ];
+  ]
+);
 
 const compile = (input, helpers) => {
     const {
         appendRaw,
-        _addComment,
         wait,
         warnings,
     } = helpers;
 
-    let currentIndex = input.tileMapIndex;
-    const currentX = [input.x, input.x + 1, input.x, input.x + 1];
-    const currentY = [input.y, input.y, input.y + 1, input.y + 1];
+
     const loopAmount = input.tileAmount === "single" ? 1 : 4;
     const hasWait = input.waitFrames != 0
-    
-    for (let i = 0; i < input.frames; i++) {
+    const frames = input.frames;
+    const items = input.items;
+    const tilemap = input.tilemapName;
 
-        for(let j = 0; j < loopAmount; j++){
-            _addComment("Swapping Tiles");
-            appendRaw(`VM_PUSH_CONST ${currentIndex}`);
-            appendRaw(`VM_REPLACE_TILE_XY ${currentX[j]}, ${currentY[j]}, ___bank_bg_${input.tilemapName}_tileset, _bg_${input.tilemapName}_tileset, .ARG0`);
-            appendRaw(`VM_POP 1`);
-            currentIndex++;
-        }
+    
+    for (let i = 0; i < frames; i++) {
+
+      for(let j = 1; j <= items; j++){
+
+          const x = input[`tile${j}_x`];
+          const y = input[`tile${j}_y`];
+          const swapX = input[`swap${j}_x`];
+          const swapY = input[`swap${j}_y`];
+
+          let currentIndex = swapY * input.tileLength + swapX + (i * loopAmount);
+          const currentX = [x, x + 1, x, x + 1];
+          const currentY = [y, y, y + 1, y + 1];
+            
+          for(let k = 0; k < loopAmount; k++){          
+              appendRaw(`VM_PUSH_CONST ${currentIndex}`);
+              appendRaw(`VM_REPLACE_TILE_XY ${currentX[k]}, ${currentY[k]}, ___bank_bg_${tilemap}_tileset, _bg_${tilemap}_tileset, .ARG0`);
+              appendRaw(`VM_POP 1`);
+              currentIndex++;
+          }
+      }
+
+        
         if(hasWait) wait(input.waitFrames)
     }
 
